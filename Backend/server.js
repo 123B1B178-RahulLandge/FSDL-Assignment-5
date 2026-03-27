d9d17dc36843d1808cd85c30b4f1bdab7f3c9d7e
Rahul <rahul.landge23@pccoepune.org>
2026-03-27 11:02:35 +0530

    enhaced css

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,6 @@ app.use(cors())
 app.use(express.json())
 
 
-
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err))
@@ -76,61 +75,6 @@ app.post("/api/book", async (req, res) => {
     }
 
 })
-
-// app.get("/api/bookings", async (req, res) => {
-
-//     try {
-
-//         const bookings = await Booking.find()
-
-//         res.json(bookings)
-
-//     } catch (error) {
-
-//         res.status(500).json({ message: error.message })
-
-//     }
-
-// })
-
-// app.get("/api/bookings/:id", async (req, res) => {
-
-//     try {
-
-//         const booking = await Booking.findById(req.params.id)
-
-//         if (!booking) {
-
-//             return res.status(404).json({ message: "Booking not found" })
-//         }
-
-//         res.json(booking)
-
-//     } catch (error) {
-
-//         res.status(500).json({ message: error.message })
-
-//     }
-
-// })
-
-// app.delete("/api/bookings/:id", async (req, res) => {
-
-//     try {
-
-//         await Booking.findByIdAndDelete(req.params.id)
-
-//         res.json({
-//             message: "Booking cancelled"
-//         })
-
-//     } catch (error) {
-
-//         res.status(500).json({ message: error.message })
-
-//     }
-
-// })
 
 const PORT = process.env.PORT || 3000
 
